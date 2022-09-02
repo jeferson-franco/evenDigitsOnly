3401c7c337d5e5c05f56904be7542bf1d86901fc
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 16:33:00 -0300

    update evenDigitsOnly.js

--- src/evenDigitsOnly.js
+++ src/evenDigitsOnly.js
@@ -1,5 +1,8 @@
 function solution(n) {
-    return n.toString().split('').every((x) => x % 2 === 0);
+    return n
+        .toString()
+        .split('')
+        .every((x) => x % 2 === 0);
 }
 
 module.exports = solution;
@@ -8,3 +11,24 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test evenDigitsOnly
+
+// alternative solution
+// function solution(n) {
+//     return !(n + '').match(/[13579]/);
+// }
+
+// alternative solution
+// function solution(n) {
+//     return (n + '').split('').every((x) => x % 2 === 0);
+// }
+
+// alternative solution
+// function solution(n) {
+//     while (n > 0) {
+//         if ((n % 10) % 2 == 1) {
+//             return false;
+//         }
+//         n = Math.floor(n / 10);
+//     }
+//     return true;
+// }
